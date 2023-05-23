fd21be5c116a8ae562bfe4cb5e618865a4c4831e
zukiin <zukinkalitshana@icloud.com>
2023-05-23 18:06:24 +0200

    Fix code for betty compliance

--- str_split.c
+++ str_split.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-char** _strtok(char *str, char *separator)
+/**
+  * _strtok - creates a string token
+  * str:
+  * seperator:
+  * Returns a token
+  */
+
+char **_strtok(char *str, char *separator)
 {
 	char **strings, *part;
 	int i = 0, j = 0;
@@ -24,7 +31,8 @@ char** _strtok(char *str, char *separator)
 		i++;
 
 		j = 0;
-		pars = strtok(NULL, " "); /* start reading from the NULL till you get to the delimiter " " */
+		pars = strtok(NULL, " ");
+		/* ^ start reading from the NULL till you get to the delimiter " " */
 	}
 	arst[i] = NULL;
 	return (arst);
--- text_prompt.c
+++ text_prompt.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-int text_prompt()
+int text_prompt(void)
 {
 	const char *text_prompt = "cisfun$ ";
 	int value;
 
 	value = (isatty(STDIN_FILENO));
 	if (value == 1)
-	write(STDOUT_FILENO, text_prompt, strlen(text_prompt)); /* create a strlen function */
+	write(STDOUT_FILENO, text_prompt, strlen(text_prompt));
+	/* above funct is to create a strlen function */
 	return (value);
 }
